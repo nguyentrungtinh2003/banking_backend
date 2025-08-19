4a0fccdce1fbe8950a96f732fe10ebb4658cb311
nguyentrungtinh <trungtinhn300@gmail.com>
2025-08-19 09:38:10 +0700

    Create controller get log by page

--- src/main/java/com/TrungTinhBackend/banking_backend/Controller/LogController.java
+++ src/main/java/com/TrungTinhBackend/banking_backend/Controller/LogController.java
@@ -3,8 +3,10 @@ package com.TrungTinhBackend.banking_backend.Controller;
 import com.TrungTinhBackend.banking_backend.Enum.LogAction;
 import com.TrungTinhBackend.banking_backend.ResponseDTO.APIResponse;
 import com.TrungTinhBackend.banking_backend.Service.Log.LogService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,5 +24,13 @@ public class LogController {
                                                  @RequestParam(defaultValue = "0") int page,
                                                  @RequestParam(defaultValue = "6") int size) {
         return ResponseEntity.ok(logService.filterLog(action,page,size));
+    }
+
+    @GetMapping("employee/log/page")
+    public ResponseEntity<APIResponse> getLogByPage(@RequestParam(defaultValue = "0") int page,
+                                                    @RequestParam(defaultValue = "6") int size,
+                                                    HttpServletRequest request,
+                                                    Authentication authentication) {
+        return ResponseEntity.ok(logService.getLogByPage(page,size,request,authentication));
     }
 }
